28f612133158f5a9de2da52ca989b41f36921084
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-10 20:47:02 +0800

    Set up files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import { getMonth } from "./util";
+
+function App() {
+	console.table(getMonth());
+	return (
+		<>
+			<h1>Hello</h1>
+		</>
+	);
+}
+
+export default App;
---
+++ src/util.js
@@ -0,0 +1,14 @@
+import dayjs from "dayjs";
+//dayjs().month() is the default value if there is nothing passed
+export function getMonth(month = dayjs().month()) {
+	const year = dayjs().year; //represent current year
+	const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day();
+	let currentMonthCount = 0 - firstDayOfTheMonth;
+	const daysMatrix = new Array(5).fill([]).map(() => {
+		return new Array(7).fill(null).map(() => {
+			currentMonthCount++;
+			return dayjs(new Date(year, month, currentMonthCount));
+		});
+	});
+	return daysMatrix;
+}
