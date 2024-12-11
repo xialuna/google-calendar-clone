d56820bd9ffb5685c65834801d645172f1dbe51e
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-12 01:08:58 +0800

    Highlight current day in calendar

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,16 +1,22 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import dayjs from "dayjs";
 
 export default function Day({ day, rowIndex }) {
+	const getCurrentDayClass = () => {
+		return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+			? "bg-primary text-white rounded-full w-7"
+			: "";
+	};
 	return (
 		<div className="flex flex-col border border-gray-200">
-			<header className="flex flex-col gap-0.5 items-center text-sm p-1 my-1">
+			<header className="flex flex-col gap-0.5 items-center text-sm text-center">
 				{rowIndex === 0 && (
-					<p className="text-graySecondary">
+					<p className="text-graySecondary mt-1">
 						{day.format("ddd").toUpperCase()}
 					</p>
 				)}
-				<p>{day.format("DD")}</p>
+				<p className={`p-1 my-1 ${getCurrentDayClass()}`}>{day.format("DD")}</p>
 			</header>
 		</div>
 	);
