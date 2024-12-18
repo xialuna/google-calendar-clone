21dd061bdbf46d41c59c932bf7882b67a6d0b001
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-18 18:29:23 +0800

    Add logic to Today button to reset to current month

--- src/components/CalenderHeader.jsx
+++ src/components/CalenderHeader.jsx
@@ -6,6 +6,11 @@ import dayjs from "dayjs";
 
 export default function CalendarHeader() {
 	const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+
+	function handleReset() {
+		setMonthIndex(dayjs().month()); //Reset to current month
+	}
+
 	function handlePrevMonth() {
 		setMonthIndex(monthIndex - 1);
 	}
@@ -17,7 +22,10 @@ export default function CalendarHeader() {
 		<header className="flex items-center px-4 py-2 gap-4">
 			<img src={logo} alt="Google Calendar Logo" className="w-11 h-11"></img>
 			<h1 className="text-2xl font-thin text-graySecondary">Calendar</h1>
-			<button className="border rounded-full py-2 px-7 border-graySecondary">
+			<button
+				onClick={handleReset}
+				className="border rounded-full py-2 px-7 border-graySecondary"
+			>
 				Today
 			</button>
 			<div className="flex gap-3 text-graySecondary">
