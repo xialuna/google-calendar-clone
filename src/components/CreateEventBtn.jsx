b2539ddac5965df3779946374d2f6a5cf81213f4
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-21 04:57:54 +0800

    Change create  event button opening holiday popover

--- src/components/CreateEventBtn.jsx
+++ src/components/CreateEventBtn.jsx
@@ -3,10 +3,10 @@ import { Plus } from "lucide-react";
 import GlobalContext from "../context/GlobalContext";
 
 const CreateEventBtn = () => {
-	const { setShowHolidayPopOver } = useContext(GlobalContext);
+	// const { setEventPopOver } = useContext(GlobalContext);
 	return (
 		<button
-			onClick={() => setShowHolidayPopOver(true)}
+			// onClick={() => setEventPopOver(true)}
 			className="flex flex-column gap-2 py-2 px-4 items-center bg-white shadow shadow-gray-400 rounded-full  hover:bg-gray-200 mb-6"
 		>
 			<Plus className="w-5" />
